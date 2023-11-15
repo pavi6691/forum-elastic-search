077a9cd6283a24a6690da0104ff5dda78dee8ebe
KIMHRAC <kimhrac@gmail.com>
2023-11-15 11:37:48 +0100

    Updated RestExceptionCA

--- services/rest-service/src/main/java/com/acme/poc/notes/exceptions/RestExceptionCA.java
+++ services/rest-service/src/main/java/com/acme/poc/notes/exceptions/RestExceptionCA.java
@@ -1,4 +1,6 @@
 package com.acme.poc.notes.exceptions;
+
+import com.acme.poc.notes.util.LogUtil;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.elasticsearch.RestStatusException;
@@ -10,16 +12,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 import java.net.SocketTimeoutException;
 
 @Slf4j
 @RestControllerAdvice
 public class RestExceptionCA {
-    
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RestStatusException.class)
     public BaseRestException handleRestException(RestStatusException exception) {
-        log.error(exception.getMessage());
+        log.error("{} {}", LogUtil.method(), exception.getMessage());
         return new BaseRestException(HttpStatus.resolve(exception.getStatus()).name(),exception.getMessage());
     }
 
@@ -29,7 +32,7 @@ public class RestExceptionCA {
         FieldValidationException ex = new FieldValidationException(HttpStatus.BAD_REQUEST.name(), 
                 "Invalid request param value");
         ex.getFields().put(exception.getName(),exception.getValue().toString());
-        log.error(ex.getMessage());
+        log.error("{} {}", LogUtil.method(), ex.getMessage());
         return ex;
     }
 
@@ -43,27 +46,25 @@ public class RestExceptionCA {
             String errorMessage = fieldError.getDefaultMessage();
             ex.getFields().put(fieldName,errorMessage);
         }
-        log.error(ex.getMessage());
+        log.error("{} {}", LogUtil.method(), ex.getMessage());
         return ex;
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(SocketTimeoutException.class)
     public BaseRestException EsTimeOutException(SocketTimeoutException exception) {
-        log.error(exception.getMessage());
+        log.error("{} {}", LogUtil.method(), exception.getMessage());
         return new BaseRestException(HttpStatus.BAD_REQUEST.name(), "Time out from elastic search. " + exception.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public BaseRestException handlePayloadFieldMisMatchException(HttpMessageNotReadableException exception) {
-        if(exception.getCause() instanceof InvalidFormatException) {
+        if (exception.getCause() instanceof InvalidFormatException) {
             InvalidFormatException invalidFormatException = (InvalidFormatException) exception.getCause();
-            FieldValidationException fieldEx = new FieldValidationException(HttpStatus.BAD_REQUEST.name(),
-                    "Invalid value in the payload");
-            fieldEx.getFields().put(invalidFormatException.getPath().size() > 0 ?  invalidFormatException.getPath().get(0).getFieldName() : "",
-                    invalidFormatException.getValue().toString());
-            log.error(fieldEx.getMessage());
+            FieldValidationException fieldEx = new FieldValidationException(HttpStatus.BAD_REQUEST.name(), "Invalid value in the payload");
+            fieldEx.getFields().put(invalidFormatException.getPath().size() > 0 ?  invalidFormatException.getPath().get(0).getFieldName() : "", invalidFormatException.getValue().toString());
+            log.error("{} {}", LogUtil.method(), fieldEx.getMessage());
             return fieldEx;
         }
         return null;
