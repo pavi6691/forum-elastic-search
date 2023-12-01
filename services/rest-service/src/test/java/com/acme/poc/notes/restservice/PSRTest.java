4d1d8442ba2df64aca9b7dc790a59c22a6de2888
KIMHRAC <kimhrac@gmail.com>
2023-12-01 15:22:31 +0100

    Re-enabled testcontainers in PSRTest

--- services/rest-service/src/test/java/com/acme/poc/notes/restservice/PSRTest.java
+++ services/rest-service/src/test/java/com/acme/poc/notes/restservice/PSRTest.java
@@ -13,8 +13,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -34,6 +42,55 @@ public class PSRTest extends BaseTest {
     @Value("${com.acme.poc.notes.test.number-of-entries:10}")
     private int NUMBER_OF_ENTRIES;
     static final UUID EXTERNAL_GUID = UUID.fromString("164c1633-44f0-4eee-8491-d5e6a5391300");
+
+
+    private static final String POSTGRESQL_IMAGE = "postgres:15.5-alpine";
+    private static final String ELASTICSEARCH_IMAGE = "docker.elastic.co/elasticsearch/elasticsearch:6.8.12";
+
+    @Container
+    public static final PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer(POSTGRESQL_IMAGE)
+            .withDatabaseName("acme")
+            .withUsername("postgresql-username")
+            .withPassword("postgresql-password");
+    @Container
+    public static final ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer(ELASTICSEARCH_IMAGE);
+
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        postgresqlContainer
+                .withNetworkAliases("postgresql");
+        postgresqlContainer.start();
+
+        elasticsearchContainer
+                .withNetworkAliases("elasticsearch")
+                .setWaitStrategy((new LogMessageWaitStrategy())
+                        .withRegEx(".*(\"message\":\\s?\"started[\\s?|\"].*|] started\n$)")
+                        .withStartupTimeout(Duration.ofSeconds(180L)));
+        elasticsearchContainer.start();
+
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", () -> "postgresql-username");
+        registry.add("spring.datasource.password", () -> "postgresql-password");
+        registry.add("elasticsearch.host", () -> elasticsearchContainer.getHost() + ":" + elasticsearchContainer.getMappedPort(9200));
+        registry.add("elasticsearch.clustername", () -> "");
+        registry.add("index.name", () -> "note-v1");
+        registry.add("default.number.of.entries.to.return", () -> 20);
+        registry.add("service.thread.pool.size", () -> 8);
+    }
+
+    @BeforeAll
+    void setup() {
+        elasticsearchContainer
+                .withNetworkAliases("elasticsearch")
+                .setWaitStrategy((new LogMessageWaitStrategy())
+                        .withRegEx(".*(\"message\":\\s?\"started[\\s?|\"].*|] started\n$)")
+                        .withStartupTimeout(Duration.ofSeconds(180L)));
+        elasticsearchContainer.start();
+
+//        assertEquals(notesAdminService.createIndex(indexName), indexName);   // TODO This does not validate correctly
+    }
+
 
     @Test
     @Order(1)
